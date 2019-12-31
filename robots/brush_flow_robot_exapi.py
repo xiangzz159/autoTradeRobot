e38da2e6af33daf7f3a781ea0bbe7172ff431689
Xiang <xiangguangye@sunline.cn>
2019-12-31 10:35:27 +0800

    commit

--- robots/brush_flow_robot_exapi.py
+++ robots/brush_flow_robot_exapi.py
@@ -220,9 +220,9 @@ class BrushFlowRobot(ExApiRobot):
             task = []
             task.append(asyncio.ensure_future(self.trades_scheduler()))
             task.append(asyncio.ensure_future(self.orderbook_scheduler()))
-            task.append(asyncio.ensure_future(self.main_scheduler()))
             task.append(asyncio.ensure_future(self.cancel_open_order_scheculer()))
             task.append(asyncio.ensure_future(self.fetch_balance()))
+            task.append(asyncio.ensure_future(self.main_scheduler()))
             # task.append(asyncio.ensure_future(self.open_disk_scheduler()))
             # task.append(asyncio.ensure_future(self.order_scheduler()))
             self.async_task(task)
