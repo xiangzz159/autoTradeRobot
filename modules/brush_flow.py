cf0cae8b3698c29d4f81ffec62dbd23b1e4cd86e
xiangguangye <xiangguangye@sunline.com>
2020-01-01 13:02:17 +0800

    update print

--- modules/brush_flow.py
+++ modules/brush_flow.py
@@ -36,13 +36,13 @@ class BrushFlow(object):
             if t - trade['timestamp'] / 1000 < 10:
                 trade_count += 1
         if trade_count > 10:
-            self.logger.info("Trade count more than 10, stop trade")
+            self.logger.debug("Trade count more than 10, stop trade")
             return None
 
         bid = bids[0][0]  # 买1
         ask = asks[0][0]  # 卖1
         if Decimal(str(ask)) - Decimal(str(bid)) == Decimal(str(self.minumun_price)):
-            self.logger.info("No insert price for disk, stop trade")
+            self.logger.debug("No insert price for disk, stop trade")
             return None
 
         p = random.uniform(bid + self.minumun_price, ask - self.minumun_price)
