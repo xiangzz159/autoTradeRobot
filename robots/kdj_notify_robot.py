702526bfc05bcb1bb366269e4867f1288b0b1e3f
Xiang <xiangguangye@sunline.cn>
2020-12-05 10:19:05 +0800

    commit

--- robots/kdj_notify_robot.py
+++ robots/kdj_notify_robot.py
@@ -34,16 +34,17 @@ class KdjNotifyRobot(ExApiRobot):
 
     async def __fetch_ohlcv(self, timeframe='15m'):
         if self.exapi:
-            kline = self.exapi.fetch_ohlcv(self.symbol, timeframe)
-            kline_ = self.kline.get(timeframe)
-            if kline_ is not None:
-                if kline[-1][0] != kline_[-1][0]:
-                    self.kline[timeframe] = kline
-                    self.logger.info('update kline, key:%s' % timeframe)
-            else:
-                self.kline[timeframe] = kline
-                self.logger.info('add kline, key:%s' % timeframe)
-            # self.logger.info("kline keys:%s" % str(self.kline.keys()))
+            for symbol in self.symbols:
+                kline = self.exapi.fetch_ohlcv(symbol, timeframe)
+                kline_ = self.kline.get(timeframe)
+                if kline_ is not None:
+                    if kline[-1][0] != kline_[-1][0]:
+                        self.kline[symbol + '_' + timeframe] = kline
+                        self.logger.info('update kline, key:%s' % timeframe)
+                else:
+                    self.kline[symbol + '_' + timeframe] = kline
+                    self.logger.info('add kline, key:%s' % timeframe)
+                # self.logger.info("kline keys:%s" % str(self.kline.keys()))
 
     def __notify(self, val1, val2, val3, val4):
         logging.info('send sms:%s', val1 + " " + val2 + " " + val3 + " " + val4)
@@ -64,7 +65,7 @@ class KdjNotifyRobot(ExApiRobot):
                         v_ = self.notifyed.get(k)
                         if v_ is None or v['last_timestamp'] != v_['last_timestamp']:
                             # SMS通知
-                            result = self.__notify(v['val1'], v['val2'], v['val3'], v['val4'])
+                            result = self.__notify(v['val1'], v['val2'], v['val3'], v['val4'], v['val5'])
                             self.logger.debug("get sms response:%s" % result)
                             # 放入已通知列表
                             self.logger.info("notifyed update key:%s" % k)
@@ -82,28 +83,30 @@ class KdjNotifyRobot(ExApiRobot):
             self.logger.debug("kdj_schedule keys:%s" % str(self.kline.keys()))
             try:
                 for k in self.kline.keys():
-                    kl = self.kline.get(k)
-                    self.logger.debug("kdj_schedule-key:%s, kline:%s" % (k, str(kl[-1])))
-                    notify = self.notifies.get(k)
-                    if self.kline.get(k) is None:
-                        continue
-                    if notify is not None and notify.get('last_timestamp') == kl[-1][0]:
-                        continue
-                    row = KDJ.analyze(kl)
-                    self.logger.debug('kdj_schedule-timestamp:%s, signal:%s' % (str(row['timestamp']), row['signal']))
-                    if row['signal'] != 'wait':
-                        self.logger.info(
-                            'kdj_schedule-timestamp:%s, signal:%s' % (str(row['timestamp']), row['signal']))
-                        signal = '金' if row['signal'] == 'long' else '死'
-                        ex_name = self.exapi.id
-                        self.notifies[k] = {
-                            'val1': ex_name,
-                            'val2': self.symbol,
-                            'val3': signal,
-                            'val4': k,
-                            'last_timestamp': kl[-1][0]
-                        }
-                        self.logger.debug("kdj_schedule-update notifies: %s" % str(self.notifies))
+                    for symbol in self.symbols:
+                        kl = self.kline.get(symbol + '_' + k)
+                        self.logger.debug("kdj_schedule-key:%s, kline:%s" % (k, str(kl[-1])))
+                        notify = self.notifies.get(symbol + '_' + k)
+                        if kl is None:
+                            continue
+                        if notify is not None and notify.get('last_timestamp') == kl[-1][0]:
+                            continue
+                        row = KDJ.analyze(kl)
+                        self.logger.debug('kdj_schedule-timestamp:%s, signal:%s' % (str(row['timestamp']), row['signal']))
+                        if row['signal'] != 'wait':
+                            self.logger.info(
+                                'kdj_schedule-timestamp:%s, signal:%s' % (str(row['timestamp']), row['signal']))
+                            signal = '金' if row['signal'] == 'long' else '死'
+                            ex_name = self.exapi.id
+                            self.notifies[symbol + '_' + k] = {
+                                'val1': ex_name,
+                                'val2': symbol,
+                                'val3': signal,
+                                'val4': str(row['close']),
+                                'val5': k,
+                                'last_timestamp': kl[-1][0]
+                            }
+                            self.logger.debug("kdj_schedule-update notifies: %s" % str(self.notifies))
             except:
                 self.logger.error("kdj schedule fail:%s" % str(traceback.format_exc()))
             finally:
@@ -131,8 +134,8 @@ class KdjNotifyRobot(ExApiRobot):
         if not self.module:
             self.logger.error("module can not be empty!")
             return
-        if not self.symbol:
-            self.logger.error("symbol can not be empty!")
+        if not self.symbols:
+            self.logger.error("symbols can not be empty!")
             return
         try:
             self.is_ready = True
@@ -164,7 +167,7 @@ def main(exapi):
     module.logger = logger
     robot = KdjNotifyRobot(exapi, module, {
         'logger': logger,
-        'symbol': 'BTC/USDT',
-        'template_id': '792028'
+        'symbols': ['BTC/USDT', 'ETH/USDT'],
+        'templateId': '794664'
     })
     robot.start()
